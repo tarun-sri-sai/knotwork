32f8f11f99a6d24bbcac9c1b382ac28b179b5fe9
tarun-sri-sai <76744890+tarun-sri-sai@users.noreply.github.com>
2026-07-11 10:53:35 +0530

    remove nextjs and supabase app components

--- components/ErrorMessage.tsx
+++
@@ -1,9 +0,0 @@
-const ErrorMessage = ({ message }: { message: string }) => {
-  return (
-    <div className="px-4 py-2 rounded-xl bg-red-500/10 text-red-700">
-      {message}
-    </div>
-  );
-};
-
-export default ErrorMessage;
