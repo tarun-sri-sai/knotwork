9d043caf38de5d524990e39c1c868177787245ba
Tuhin Tarafder <ttarafder7d1@protonmail.com>
2026-07-14 07:25:50 +0530

    update .gitignore to ignore /result and enhance flake.nix with vendorHash and postInstall for knotwork and knotwork-mcp

--- flake.nix
+++ flake.nix
@@ -10,7 +10,35 @@
       top@{ ... }:
       {
         systems = [ "x86_64-linux" ];
-        perSystem = { pkgs, ... }: {
+        perSystem = { lib, pkgs, ... }: {
+          packages = rec {
+            knotwork = pkgs.buildGoModule {
+              pname = "knotwork";
+              version = "0.1.0";
+
+              src = ./core;
+              vendorHash = "sha256-7AaW3amqIzITMgIFrKoqc5JJkVDcsGqFynL38Sk0fB4=";
+
+              postInstall = ''
+                mv $out/bin/app $out/bin/knotwork
+              '';
+            };
+
+            knotwork-mcp = pkgs.buildGoModule {
+              pname = "knotwork-mcp";
+              version = "0.1.0";
+
+              src = ./mcp;
+              vendorHash = "sha256-hbV/kOuImCWwmxcOdg9bEM8VLrCcn0m5bF4MMmj9lSs=";
+
+              postInstall = ''
+                mv $out/bin/app $out/bin/knotwork-mcp
+              '';
+            };
+
+            default = knotwork;
+          };
+
           devShells.default = pkgs.mkShell {
             packages = with pkgs; [
               nixd
